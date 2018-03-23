a54f84cbe95a7e6cbfe74198a23f70683e85c384
wuhencut <q.com1076668563@qq.com>
2018-03-23 13:22:45 +0800

    基本完成点餐系统

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: './',//公用路径（绝对路径不可用）
 
     /**
      * Source Maps
